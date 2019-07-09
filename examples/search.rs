5ebba3b28c1cedd93b1c57249e7e2a08754fbcb1
Edouard Paris <m@edouard.paris>
2019-07-09 13:24:07 +0200

    refac examples: search

--- examples/search.rs
+++ examples/search.rs
@@ -1,10 +1,13 @@
 use futures::future::{lazy};
 use actix_rt::System;
 
+use ulule::client::Client;
+use ulule::search;
+
 fn main() {
-    let client = ulule::client::Client::new();
-    let p: ulule::search::Projects = System::new("test").block_on(lazy(|| {
-        ulule::search::projects(&client, Some(ulule::search::Params::new().limit(30)))
+    let client = Client::new();
+    let p: search::Projects = System::new("test").block_on(lazy(|| {
+        search::projects(&client, Some(search::Params::new().limit(30)))
     })).unwrap();
     println!("{:?}", p)
 }
