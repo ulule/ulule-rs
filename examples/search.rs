d1bf90572ee3e20d17c6bcd091b5c2fdde5e71e2
Edouard Paris <m@edouard.paris>
2019-07-12 15:32:21 +0200

    fix exemple: change param scope

--- examples/search.rs
+++ examples/search.rs
@@ -6,14 +6,14 @@ use ulule::search;
 
 fn main() {
     let client = Client::new();
+    let p = search::Params::new()
+        .limit(3)
+        .with_extra_fields(
+            vec![
+            "owner".to_string(),
+            "main_tag".to_string(),
+            "main_image".to_string()]);
     let p: search::Projects = System::new("test").block_on(lazy(|| {
-        let p = search::Params::new()
-            .limit(3)
-            .with_extra_fields(
-                vec![
-                "owner".to_string(),
-                "main_tag".to_string(),
-                "main_image".to_string()]);
         search::projects(&client, Some(p))
     })).unwrap();
     println!("{:?}", p)
